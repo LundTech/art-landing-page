573fef3dc85c3794397d324f78ac00f4241b2bec
Jonas Lundholm <jonas.luundholm@gmail.com>
2023-08-14 18:23:12 +0200

    Cleaned up old demo content

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
-import Experience from "../pages/experience/Experience";
-import Contact from "../pages/contact/Contact";
-import ProjectsPage from "../pages/projects/ProjectsPage";
-import ContactPage from "../pages/contact/Contact";
-import Thanks from "../pages/thanks/Thanks";
 
 export default class Main extends Component {
   render() {
     return (
       <>
-        <HashRouter basename="/">
+        <BrowserRouter basename="/">
           <Switch>
             <Route
               path="/"
@@ -22,37 +17,8 @@ export default class Main extends Component {
               path="/home"
               render={(props) => <Home {...props} theme={this.props.theme} />}
             />
-            <Route
-              path="/experience"
-              exact
-              render={(props) => (
-                <Experience {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
-              path="/contact"
-              render={(props) => (
-                <Contact {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
-              path="/projects"
-              render={(props) => (
-                <ProjectsPage {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
-              path="/kontakt"
-              render={(props) => (
-                <ContactPage {...props} theme={this.props.theme} />
-              )}
-            />
-            <Route
-              path="/tack"
-              render={(props) => <Thanks {...props} theme={this.props.theme} />}
-            />
           </Switch>
-        </HashRouter>
+        </BrowserRouter>
       </>
     );
   }
