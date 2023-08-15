3a31c7c85517c73d8826422de1dd384e2a9bf37b
Jonas Lundholm <jonas.luundholm@gmail.com>
2023-08-15 14:31:22 +0200

    Experiment with removing credits

--- src/components/backgroundImage/BackgroundImage.js
+++ src/components/backgroundImage/BackgroundImage.js
@@ -1,24 +1,38 @@
 import React, { Component } from "react";
 import "./BackgroundImage.css";
 import { Fade } from "react-reveal";
-import Credit from "../../components/credit/Credit.js"
+import Credit from "../../components/credit/Credit.js";
 
 class BackgroundImage extends Component {
   render() {
-    const onRSVPButtonPress= this.props.onRSVPButtonPress;
+    const onRSVPButtonPress = this.props.onRSVPButtonPress;
     return (
       <div>
         <Fade>
-            <div className="bg-image">
-              <div className="bg-image-text-container" style={{ display: 'flex', flexDirection: 'column' }}>
-                <span className="bg-image-text">Experience <br/>art </span>
-                <span className="bg-image-text-subtitle">18th of August 7PM EST <br/>East West Gallery Street</span>
-                <div className="bg-image-btn-container">
-                  <input className="bg-image-btn" type="button" value={"Sign up"} onClick={onRSVPButtonPress}></input>
-                </div>
+          <div className="bg-image">
+            <div
+              className="bg-image-text-container"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <span className="bg-image-text">
+                Experience <br />
+                art{" "}
+              </span>
+              <span className="bg-image-text-subtitle">
+                18th of August 7PM EST <br />
+                East West Gallery Street
+              </span>
+              <div className="bg-image-btn-container">
+                <input
+                  className="bg-image-btn"
+                  type="button"
+                  value={"Sign up"}
+                  onClick={onRSVPButtonPress}
+                ></input>
               </div>
-            <Credit/>
             </div>
+            {/* <Credit/> */}
+          </div>
         </Fade>
       </div>
     );
